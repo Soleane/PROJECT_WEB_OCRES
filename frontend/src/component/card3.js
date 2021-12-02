69adcb246f5fca0e7bbe24db80c201891dd27c65
Soleane <soleane.denis@edu.ece.fr>
2021-12-03 00:10:21 +0100

    api

--- frontend/src/component/card3.js
+++ frontend/src/component/card3.js
@@ -1,6 +1,55 @@
 import React from "react";
+import axios from 'axios';
+
+//API KEY: 4fa22870f4233e24dc25c2741af4c7c3
+
+// Clé api
+const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
+// Url API
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+// Base source icon
+const API_URL_ICON = "http://openweathermap.org/img/wn/";
+
+
 export default class Dashboard extends React.Component 
 {
+	constructor(props)
+	{
+	   super(props);
+	   this.state = {
+            recherche : "Paris",
+            meteo : undefined,
+            date: new Date()
+        }
+    }
+   
+
+    getmeteo()
+    {
+        axios.get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`)
+        .then(res => {
+            const meteo = res.data;
+            this.setState({ meteo : meteo });
+        })
+    }
+    componentDidMount() { 
+        this.getmeteo();
+        this.timerID = setInterval(
+            () => this.tick(), 
+            1000 
+        ); 
+    } 
+
+    
+    componentWillUnmount() { 
+        clearInterval(this.timerID); 
+    } 
+    
+    tick() { 
+        this.setState({ 
+            date: new Date() 
+        }); 
+    } 
 
     render()
     {
@@ -9,14 +58,11 @@ export default class Dashboard extends React.Component
             <div>
                 <div className="card3">
                 <card3 className="App-.card3">
-                    <p>
-                        -  Your classement :
-                    </p>
+                <h2> {this.state.date.toLocaleDateString()} </h2>
+                <h4>{this.state.date.toLocaleTimeString()}. h </h4>
                 </card3>
             </div>               
             </div>
         )
-            
     }
-
 }
