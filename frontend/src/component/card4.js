fa3481624eff40b7b70203f17714defeb0b61e82
Soleane <soleane.denis@edu.ece.fr>
2021-12-03 15:41:34 +0100

    new

--- frontend/src/component/card4.js
+++ frontend/src/component/card4.js
@@ -1,7 +1,6 @@
 import React from "react";
 import youtube from './youtube';
 import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
 
 export default class Dashboard extends React.Component 
 {
